caf48ed93d75222cae0d48b54e6c1fe28868b4ca
Abhishek Pakhare <abhishekpakhare1997@gmail.com>
2024-09-23 00:12:13 +0530

    fix/ui: made gride responsive

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,11 +3,10 @@ import { useLoaderData } from "react-router-dom";
 import { AgentCard } from "../components/AgentCard";
 
 export const Home = () => {
- 
   const { data } = useLoaderData();
   return (
     <>
-      <div className="grid grid-cols-3 justify-items-center">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 mx-10">
         {data.map((agent) => {
           return <AgentCard key={agent.uuid} agent={agent} />;
         })}
